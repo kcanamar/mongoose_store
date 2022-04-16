d3c13474e064df87d703bef3e4b2f4db4ecc13ff
Kyle Canamar <canamar.kyle@gmail.com>
2022-04-16 15:57:23 -0700

    establishes working seed route with seed model

--- controllers/store.js
+++ controllers/store.js
@@ -1,53 +1,58 @@
 ////////////////////////
 // Setup - Import deps and create app object
 ////////////////////////
-const express = require('express')
-const storeRouter = express.Router()
+const express = require('express');
+const Product = require('../models/products');
+const productsSeed = require('../models/seed');
+const storeRouter = express.Router();
 ///////////////////////
 // Declare Routes and Routers 
 ///////////////////////
 // INDUCES - Index, New, Delete, Update, Create, Edit, Show, Seed
 // Index Route
 storeRouter.get("/", (req, res) => {
-    console.log("The store is open")
-    res.send("Welcome to the store")
-})
+    console.log("The store is open");
+    res.send("Welcome to the store");
+});
 // New Route
 storeRouter.get("/new", (req, res) => {
-    console.log("A New Chllanger Approaches")
-    res.send("New LTO...")
-})
+    console.log("A New Chllanger Approaches");
+    res.send("New LTO...");
+});
 // Seed Route
 storeRouter.get("/seed", (req, res) => {
-    console.log("Seed you lawn")
-    res.send("Seeded......")
-})
+    Product.deleteMany({}, (err, deletedProducts) => {
+        Product.create(productsSeed, (err, data) => {
+            res.send(productsSeed);
+        });
+    });
+});
 // Delete Route
 storeRouter.delete("/:id", (req, res) => {
-    console.log("Target Neutralized..." + req.params.id)
-    res.send("Deleted...." + req.params.id)
-})
+    console.log("Target Neutralized..." + req.params.id);
+    res.send("Deleted...." + req.params.id);
+});
 // Update Route
 storeRouter.put('/:id', (req, res) => {
-    console.log("Upgrade Complete..." + req.params.id)
-    res.send("Upgrade Merge Complete..." + req.params.id) 
-})
+    console.log("Upgrade Complete..." + req.params.id);
+    res.send("Upgrade Merge Complete..." + req.params.id); 
+});
 // Create Route
 storeRouter.post("/", (req, res) => {
-    console.log("You Must Contruct Additional Pylons." + req.body.name)
-    res.send("Look its a baby" + req.body.name)
-})
+    console.log("You Must Contruct Additional Pylons." + req.body.name);
+    res.send("Look its a baby" + req.body.name);
+});
 // Edit Route
 storeRouter.get("/:id/edit", (req, res) => {
-    console.log("Edit this one" + req.params.id)
-    res.send("Okay who is getting a new hair cut?" + req.params.id)
-})
+    console.log("Edit this one" + req.params.id);
+    res.send("Okay who is getting a new hair cut?" + req.params.id);
+});
 // Show Route
 storeRouter.get("/:id", (req, res) => {
-    console.log("I have chosen" + req.params.id)
-    res.send("Chosen One!... I'm Coming!" + req.params.id)
-})
+    console.log("I have chosen" + req.params.id);
+    res.send("Chosen One!... I'm Coming!" + req.params.id);
+});
 ///////////////////////////
 // Export
 ///////////////////////////
-module.exports = storeRouter
+module.exports = storeRouter;
---
+++ models/products.js
@@ -0,0 +1,18 @@
+////////////////////////
+// Setup - Import deps and create app object
+////////////////////////
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+    name: String || "N/A",
+    description: String || "N/A",
+    img: String || "N/A",
+    price: Number || 0,
+    qty: Number || 0
+});
+///////////////////////////
+// Export
+///////////////////////////
+const Product = mongoose.model("Product", productSchema)
+module.exports = Product
