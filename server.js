63bcf56f490bbb267637d69bea79ebce6d75da18
Kyle Canamar <canamar.kyle@gmail.com>
2022-04-16 13:31:52 -0700

    establishes server listening on PORT, establishes working mongoDB connection

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 ////////////////////////
 // Setup - Import deps and create app object
 ////////////////////////
-
+const express = require('express');
+const mongoose = require('mongoose');
+// const methodOverride = require('method-override');
+// const storeController = require('./controllers/store');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3001;
 //////////////////////
 // Declare Middleware
 //////////////////////
@@ -14,3 +20,13 @@
 ///////////////////////////
 // Server Listener
 ///////////////////////////
+//MongoDB & Mongoose
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + "oops that didnt work"));
+db.on('connected', () => console.log("mongoose connected"));
+db.on('disconnected', () => console.log("mongoose disconnected"));
+app.listen(PORT, () => console.log("Hello Welcome..."));
